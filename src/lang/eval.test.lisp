d3b48cb8cf16f3ded582852b2e1fca46acab3d12
Xie Yuheng <xyheme@gmail.com>
2025-08-21 03:50:09 +0800

    [lang] eval.test instead of eval.snapshot

---
+++ src/lang/eval.test.lisp
@@ -0,0 +1,11 @@
+(import-all "index.lisp")
+
+(define (test-exp sexp expected)
+  (= exp (parse-exp sexp))
+  (= value (eval-exp exp empty-env))
+  (assert (equal? value expected)))
+
+(test-exp '8 8)
+(test-exp '(- 8) -8)
+(test-exp '(+ (+ 8) (- 8)) 0)
+(test-exp '(let ((x 8)) (+ x x)) 16)
