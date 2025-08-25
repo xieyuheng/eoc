f2bb48e134bc578ee1d37a09a72986fc1abceb50
Xie Yuheng <xyheme@gmail.com>
2025-08-26 04:00:46 +0800

    up

--- scripts/run-src.sh
+++ scripts/run-src.sh
@@ -4,17 +4,17 @@ bin="npx occam-lisp.js run --debug=true"
 ext=lisp
 dir=src
 
-for file in $(find $dir -name "*.${ext}" -not -name "*.snapshot.${ext}" -not -name "*.error.${ext}" -not -name "*.play.${ext}" -not -name "*.benchmark.${ext}"); do
-    echo "[run] $file"
+for file in $(find $dir -name "*.test.${ext}"); do
+    echo "[test] $file"
     ${bin} $file
 done
 
 for file in $(find $dir -name "*.snapshot.${ext}"); do
-    echo "[out] $file"
+    echo "[snapshot] $file"
     ${bin} $file > $file.out
 done
 
 for file in $(find $dir -name "*.error.${ext}"); do
-    echo "[err] $file"
+    echo "[error] $file"
     ${bin} $file > $file.err || true
 done
