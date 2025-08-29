68610bbb6e0c1f5c5ec3e3fbf252b74df744114f
Xie Yuheng <xyheme@gmail.com>
2025-08-29 15:02:41 +0800

    up

--- src/passes/010-uniquify.snapshot.lisp
+++ src/passes/010-uniquify.snapshot.lisp
@@ -1,34 +1,34 @@
 (import-all "../langs/s/index.lisp")
 (import-all "010-uniquify.lisp")
 
-(define (test-program program-sexp value)
+(define (test-program expected program-sexp)
   (= program-0 (parse-program program-sexp))
   (= program-1 (uniquify program-0))
   (= result (eval-program program-1))
   (write "000 ") (writeln (format-sexp (format-program program-0)))
   (write "010 ") (writeln (format-sexp (format-program program-1)))
-  (assert-equal result value))
+  (assert-equal expected result))
 
 (test-program
+ 4
  '(program
    ()
    (let ((x 4))
-     (- 8 x)))
- 4)
+     (- 8 x))))
 
 (test-program
+ 42
  '(program
    ()
    (let ((x 32))
      (+ (let ((x 10))
           x)
-        x)))
- 42)
+        x))))
 
 (test-program
+ 7
  '(program
    ()
    (let ((x (let ((x 4))
               (+ x 1))))
-     (+ x 2)))
- 7)
+     (+ x 2))))
