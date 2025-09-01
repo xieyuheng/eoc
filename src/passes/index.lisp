64210d667e0414f6af7187a9e5ff5b1839433a19
Xie Yuheng <xyheme@gmail.com>
2025-09-01 16:10:26 +0800

    up

--- src/passes/index.lisp
+++ src/passes/index.lisp
@@ -1,6 +1,39 @@
+;; To compile one language to another,
+;; is to bridge the differences between the two languages.
+
+;; We ease the challenge of compiling from s to x86
+;; by breaking down the problem into several steps,
+;; which deal with these differences one at a time.
+;; Each of these steps is called a pass of the compiler.
+
 (include "010-uniquify.lisp" uniquify)
+;; deals with the shadowing of variables
+;; by renaming every variable to a unique name.
+
 (include "020-remove-complex-operands.lisp" rco-program)
+;; ensures that each subexpression of a primitive operation
+;; or function call is a variable or integer,
+;; that is, an _atomic_ expression.
+;; We refer to nonatomic expressions as _complex_.
+;; This pass introduces temporary variables to hold
+;; the results of complex subexpressions.
+
 (include "030-explicate-control.lisp" explicate-control)
+;; makes the execution order of the program explicit.
+;; It converts the abstract syntax tree representation into a graph
+;; in which each node is a labeled sequence of statements
+;; and the edges are `goto` statements.
+
 (include "040-select-instructions.lisp" select-instructions)
+;; handles the difference between s operations and x86
+;; instructions. This pass converts each LVar operation to a short
+;; sequence of instructions that accomplishes the same task.
 
 ;; (include "050-assign-homes.lisp" assign-homes)
+;; replaces variables with registers or stack locations.
+
+;; (include "060-patch-instructions.lisp" patch-instructions)
+;; TODO
+
+;; (include "070-prelude-and-conclusion.lisp" prelude-and-conclusion)
+;; TODO
