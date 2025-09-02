4211e0c3087f0722caa5a746e462e7cdbc18542c
Xie Yuheng <xyheme@gmail.com>
2025-09-02 18:20:23 +0800

    up

--- src/passes/020-remove-complex-operands.lisp
+++ src/passes/020-remove-complex-operands.lisp
@@ -3,65 +3,67 @@
 (define (rco-program program)
   (match program
     ((cons-program info body)
-     (cons-program info (rco-exp body)))))
+     (cons-program info (rco-exp [:count 0] body)))))
 
-(define freshen-state [:count 0])
+(define state? (tau :count int-non-negative?))
 
-(define (freshen name)
-  (= count (record-get 'count freshen-state))
-  (record-set! 'count (iadd 1 count) freshen-state)
+(claim freshen (-> state? symbol? symbol?))
+
+(define (freshen state name)
+  (= count (record-get 'count state))
+  (record-set! 'count (iadd 1 count) state)
   (symbol-append name (string-to-symbol (format-subscript (iadd 1 count)))))
 
-(claim rco-exp (-> exp? exp?))
+(claim rco-exp (-> state? exp? exp?))
 
-(define (rco-exp exp)
+(define (rco-exp state exp)
   (match exp
     ((var-exp name)
      (var-exp name))
     ((int-exp n)
      (int-exp n))
     ((let-exp name rhs body)
-     (let-exp name (rco-exp rhs) (rco-exp body)))
+     (let-exp name (rco-exp state rhs) (rco-exp state body)))
     ((prim-exp op args)
-     (= [new-args bindings] (rco-args args))
+     (= [new-args bindings] (rco-args state args))
      (make-lets bindings (prim-exp op new-args)))))
 
 ;; we must use (list? (tau symbol? exp?)) instead of (record? exp?),
 ;; because we need to control the order of entries.
 ;; the binding of the inner arg should be cons-ed at the out side.
 ;; > (+ (+ 1 2) (+ 3 (+ 4 5)))
-;; = (let ((_₃ (+ 4 5)))
-;;     (let ((_₄ (+ 3 _₃)))
-;;       (let ((_₂ (+ 1 2)))
-;;         (+ _₂ _₄))))
+;; = (let ((_₂ (+ 4 5)))
+;;     (let ((_₃ (+ 3 _₂)))
+;;       (let ((_₁ (+ 1 2)))
+;;         (+ _₁ _₃))))
 
 (claim rco-args
-  (-> (list? exp?)
+  (-> state? (list? exp?)
       (tau (list? exp?)
            (list? (tau symbol? exp?)))))
 
-(define (rco-args args)
-  (= [new-args bindings-list] (list-unzip (list-map rco-arg args)))
+(define (rco-args state args)
+  (= [new-args bindings-list] (list-unzip (list-map (rco-arg state) args)))
   [new-args (list-append-many bindings-list)])
 
 (claim rco-arg
-  (-> exp?
+  (-> state? exp?
       (tau exp?
            (list? (tau symbol? exp?)))))
 
-(define (rco-arg arg)
+(define (rco-arg state arg)
   (match arg
     ((var-exp name)
      [(var-exp name) []])
     ((int-exp n)
      [(int-exp n) []])
     ((let-exp name rhs body)
-     (= [new-body bindings] (rco-arg body))
+     (= [new-body bindings] (rco-arg state body))
      [new-body
-      (cons [name (rco-exp rhs)] bindings)])
+      (cons [name (rco-exp state rhs)] bindings)])
     ((prim-exp op args)
-     (= [new-args bindings] (rco-args args))
-     (= name (freshen '_))
+     (= [new-args bindings] (rco-args state args))
+     (= name (freshen state '_))
      [(var-exp name)
       (cons [name (prim-exp op new-args)] bindings)])))
 
