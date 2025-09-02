8f3e03463729be417656147baa1e4a29895a1b72
Xie Yuheng <xyheme@gmail.com>
2025-09-02 16:52:54 +0800

    up

--- src/passes/020-remove-complex-operands.lisp
+++ src/passes/020-remove-complex-operands.lisp
@@ -16,39 +16,48 @@
     ((let-exp name rhs body)
      (let-exp name (rco-exp rhs) (rco-exp body)))
     ((prim-exp op args)
-     (= [new-args bindings-list] (list-unzip (list-map rco-atom args)))
-     (= bindings (list-append-many bindings-list))
+     (= [new-args bindings] (rco-args args))
      (make-lets bindings (prim-exp op new-args)))))
 
-(claim make-lets
-  (-> (list? (tau symbol? exp?)) exp? exp?))
+(claim rco-args
+  (-> (list? exp?)
+      (tau (list? exp?)
+           (list? (tau symbol? exp?)))))
 
-(define (make-lets bindings base-exp)
-  (match bindings
-    ('()
-     base-exp)
-    ((cons [name exp] rest-bindings)
-     (make-lets rest-bindings (let-exp name exp base-exp)))))
+(define (rco-args args)
+  (= [new-args bindings-list] (list-unzip (list-map rco-arg args)))
+  [new-args (list-append-many bindings-list)])
 
-(claim rco-atom
-  (-> exp? (tau exp? (list? (tau symbol? exp?)))))
+(claim rco-arg
+  (-> exp?
+      (tau exp?
+           (list? (tau symbol? exp?)))))
 
-(define (rco-atom exp)
+(define (rco-arg exp)
   (match exp
     ((var-exp name)
      [(var-exp name) []])
     ((int-exp n)
      [(int-exp n) []])
     ((let-exp name rhs body)
-     (= [new-body bindings] (rco-atom body))
+     (= [new-body bindings] (rco-arg body))
      [new-body
       (cons [name (rco-exp rhs)] bindings)])
     ((prim-exp op args)
-     (= [new-args bindings-list] (list-unzip (list-map rco-atom args)))
-     (= bindings (list-append-many bindings-list))
+     (= [new-args bindings] (rco-args args))
      (= name (freshen '_))
      [(var-exp name)
       (cons [name (prim-exp op new-args)] bindings)])))
+
+(claim make-lets
+  (-> (list? (tau symbol? exp?)) exp? exp?))
+
+(define (make-lets bindings base-exp)
+  (match bindings
+    ([]
+     base-exp)
+    ((cons [name exp] rest-bindings)
+     (make-lets rest-bindings (let-exp name exp base-exp)))))
 
 (define freshen-state [:count 0])
 
