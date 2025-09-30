34493c17124c85de19b0710c4d1ea7b1a3ea6ae3
Xie Yuheng <xyheme@gmail.com>
2025-09-30 21:44:39 +0800

    extract compile-program/level-0 compile-program/level-1

--- compiler/src/compile/compile-program.lisp
+++ compiler/src/compile/compile-program.lisp
@@ -11,22 +11,28 @@
 
 (define (compile-program optimization-level program)
   (match optimization-level
-    (0 (pipe program
-         check-program
-         (compose check-program uniquify)
-         (compose check-program rco-program)
-         (compose check-c-program explicate-control)
-         select-instructions
-         assign-homes
-         patch-instructions
-         prolog-and-epilog))
-    (1 (pipe program
-         check-program
-         (compose check-program partial-eval-program)
-         (compose check-program uniquify)
-         (compose check-program rco-program)
-         (compose check-c-program explicate-control)
-         select-instructions
-         assign-homes
-         patch-instructions
-         prolog-and-epilog))))
+    (0 (compile-program/level-0 program))
+    (1 (compile-program/level-1 program))))
+
+(define (compile-program/level-0 program)
+  (pipe program
+    check-program
+    (compose check-program uniquify)
+    (compose check-program rco-program)
+    (compose check-c-program explicate-control)
+    select-instructions
+    assign-homes
+    patch-instructions
+    prolog-and-epilog))
+
+(define (compile-program/level-1 program)
+  (pipe program
+    check-program
+    (compose check-program partial-eval-program)
+    (compose check-program uniquify)
+    (compose check-program rco-program)
+    (compose check-c-program explicate-control)
+    select-instructions
+    assign-homes
+    patch-instructions
+    prolog-and-epilog))
