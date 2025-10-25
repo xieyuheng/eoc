5e5685bcc115d83af67de0ccb986f5816f1e7d26
Xie Yuheng <xyheme@gmail.com>
2025-10-25 20:43:22 +0800

    gc_initialize

--- runtime/src/gc/gc.c
+++ runtime/src/gc/gc.c
@@ -3,27 +3,30 @@
 object_t** gc_root_stack_begin = NULL;
 object_t** gc_root_stack_end = NULL;
 
-object_t* gc_free_pointer = NULL;
-
 object_t* gc_from_space_begin = NULL;
 object_t* gc_from_space_end = NULL;
 
 static object_t* gc_to_space_begin = NULL;
 static object_t* gc_to_space_end = NULL;
 
+object_t* gc_free_pointer = NULL;
+
+static bool initialized = false;
+
 void
 gc_initialize(size_t root_stack_size, size_t heap_size) {
-    assert(sizeof(object_t) == 8);
+    if (initialized) return;
 
-    assert(root_stack_size % 8 == 0);
-    assert(heap_size % 8 == 0);
+    gc_root_stack_begin = allocate_pointers(root_stack_size);
+    gc_root_stack_end = gc_root_stack_begin + root_stack_size;
 
-    gc_root_stack_begin = allocate(root_stack_size);
-    gc_root_stack_end = gc_root_stack_begin + root_stack_size / 8;
+    gc_from_space_begin = allocate_pointers(heap_size);
+    gc_from_space_end = gc_from_space_begin + heap_size;
 
-    gc_from_space_begin = allocate(heap_size);
-    gc_from_space_end = gc_from_space_begin + heap_size / 8;
+    gc_to_space_begin = allocate_pointers(heap_size);
+    gc_to_space_end = gc_to_space_begin + heap_size;
 
-    gc_to_space_begin = allocate(heap_size);
-    gc_to_space_end = gc_to_space_begin + heap_size / 8;
+    gc_free_pointer = gc_from_space_begin;
+
+    initialized = true;
 }
--- runtime/src/gc/gc.h
+++ runtime/src/gc/gc.h
@@ -3,9 +3,10 @@
 extern object_t** gc_root_stack_begin;
 extern object_t** gc_root_stack_end;
 
-extern object_t* gc_free_pointer;
-
 extern object_t* gc_from_space_begin;
 extern object_t* gc_from_space_end;
 
+extern object_t* gc_free_pointer;
+
+// size in word (64 bits (not byte))
 void gc_initialize(size_t root_stack_size, size_t heap_size);
